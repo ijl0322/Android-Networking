7e47c009db6f2acad18cefcb641ba15fff1590b3
ijl0322 <ijl0322@gmail.com>
2018-04-18 14:44:17 -0400

    5 Http url connection

---
+++ start/app/src/main/java/com/raywenderlich/android/w00tze/repository/BasicRepository.kt
@@ -0,0 +1,93 @@
+package com.raywenderlich.android.w00tze.repository
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.net.Uri
+import android.os.AsyncTask
+import android.util.Log
+import com.raywenderlich.android.w00tze.app.Constants.fullUrlString
+import com.raywenderlich.android.w00tze.model.Gist
+import com.raywenderlich.android.w00tze.model.Repo
+import com.raywenderlich.android.w00tze.model.User
+import java.io.IOException
+
+/**
+ * Created by isabellee on 4/18/18.
+ */
+
+object BasicRepository : Repository {
+	private const val TAG = "BasicRepository"
+	private const val LOGIN = "w00tze"
+
+
+	override fun getRepos(): LiveData<List<Repo>> {
+		val liveData = MutableLiveData<List<Repo>>()
+
+		FetchReposAsyncTask({ repos ->
+				liveData.value = repos
+		}).execute()
+		return liveData
+	}
+
+	override fun getGists(): LiveData<List<Gist>> {
+		val liveData = MutableLiveData<List<Gist>>()
+		val gists = mutableListOf<Gist>()
+
+		for (i in 0 until 100) {
+			val gist = Gist("2018-02-23T17:42:52Z", "w00t")
+			gists.add(gist)
+		}
+
+		liveData.value = gists
+
+		return liveData
+	}
+
+	override fun getUser(): LiveData<User> {
+		val liveData = MutableLiveData<User>()
+
+		val user = User(
+				1234L,
+				"w00tze",
+				"w00tze",
+				"W00tzeWootze",
+				"https://avatars0.githubusercontent.com/u/36771440?v=4")
+
+		liveData.value = user
+
+		return liveData
+	}
+
+	private fun fetchRepos(): List<Repo>? {
+		try {
+			val url = Uri.parse(fullUrlString("/users/${LOGIN}/repos")).toString()
+			val jsonString = getURLAsString(url)
+
+			Log.i(TAG, "Repo data: $jsonString")
+
+			val repos = mutableListOf<Repo>()
+			for (i in 0 until 100) {
+				val repo = Repo("repo name")
+				repos.add(repo)
+			}
+
+			return repos
+		} catch (e: IOException) {
+			Log.e(TAG, "Error retrieving repos: ${e.localizedMessage}")
+		}
+		return null
+	}
+
+	private class FetchReposAsyncTask(val callback: ReposCallback) : AsyncTask<ReposCallback, Void, List<Repo>>() {
+		override fun doInBackground(vararg params: ReposCallback?): List<Repo>? {
+			return fetchRepos()
+		}
+
+		override fun onPostExecute(result: List<Repo>?) {
+			super.onPostExecute(result)
+			if (result != null) {
+				callback(result)
+			}
+		}
+	}
+}
---
+++ start/app/src/main/java/com/raywenderlich/android/w00tze/repository/BasicUtils.kt
@@ -0,0 +1,47 @@
+package com.raywenderlich.android.w00tze.repository
+
+import com.raywenderlich.android.w00tze.model.Repo
+import java.io.*
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+/**
+ * Created by isabellee on 4/13/18.
+ */
+
+internal typealias ReposCallback = (repos: List<Repo>) -> Unit
+
+@Throws(IOException::class)
+internal fun getURLAsString(urlAddress: String):String {
+	val url = URL(urlAddress)
+	val conn = url.openConnection() as HttpsURLConnection
+	conn.requestMethod = "GET"
+	conn.setRequestProperty("Accept", "application/json")
+	return try {
+		val inputStream = conn.inputStream
+		if (conn.responseCode != HttpsURLConnection.HTTP_OK) {
+			throw IOException("${conn.responseMessage} for $urlAddress")
+		}
+
+		if (inputStream != null) {
+			convertStreamToString(inputStream)
+		} else {
+			"Error retrieving $urlAddress"
+		}
+	} finally {
+		conn.disconnect()
+	}
+}
+
+@Throws(IOException::class)
+private fun convertStreamToString(inputStream: InputStream): String {
+	val reader = BufferedReader(InputStreamReader(inputStream))
+	val sb = StringBuilder()
+	var line: String? = reader.readLine()
+	while (line != null) {
+		sb.append(line).append("\n")
+		line = reader.readLine()
+	}
+	reader.close()
+	return sb.toString()
+}
